6b485200fedad4d3554b909786737166267780c2
p4pu <domingoperezguerrero@gmail.com>
2018-04-20 23:43:58 +0200

    adminpro Template demo

---
+++ src/app/shared/errors/errors.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-errors',
+  templateUrl: './errors.component.html',
+  styles: []
+})
+export class ErrorsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
